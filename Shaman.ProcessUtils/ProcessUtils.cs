eaa62963e4f4b6cf0154ec4660c92ac8b5890056
Andrea Martinelli <andmartinelli@gmail.com>
2017-03-14 22:37:39 +0100

    PATH lookup, RunFromRaw

--- Shaman.ProcessUtils/ProcessUtils.cs
+++ Shaman.ProcessUtils/ProcessUtils.cs
@@ -23,6 +23,18 @@ namespace Shaman.Runtime
             //    }
 
             //}
+
+
+            var resolved = PathLookup(command, null);
+            if (resolved == null) throw new FileNotFoundException("Cannot find application: " + command, command);
+            if (resolved.EndsWith(".cmd", StringComparison.OrdinalIgnoreCase) || resolved.EndsWith(".bat", StringComparison.OrdinalIgnoreCase))
+            {
+                args = new[] { "/c", command }.Concat(args).ToArray();
+                command = "cmd.exe";
+            }
+
+
+
             psi.FileName = command;
             psi.WorkingDirectory = workingDirectory;
             psi.Arguments = GetArguments(args);
@@ -35,6 +47,30 @@ namespace Shaman.Runtime
             return str.Any(y => !(char.IsLetterOrDigit(y) || y == '\\' || y == '|' || y == '/' || y == '.' || y == ':' || y == '-'));
         }
 
+        public static string PathLookup(string file, string additionalSearchFolder)
+        {
+            if (file.Contains("/") || file.Contains("\\")) return file;
+            if (file == "cmd.exe") return file;
+            IEnumerable<string> paths = Environment.GetEnvironmentVariable("path").Split(';').Select(x => Environment.ExpandEnvironmentVariables(x)).ToList();
+            if (additionalSearchFolder != null) paths = new[] { additionalSearchFolder }.Concat(paths);
+
+            foreach (var p_ in paths)
+            {
+                var p = p_;
+                if (!p.EndsWith("\\")) p += "\\";
+                var pfile = p + file;
+                if (File.Exists(pfile))
+                {
+                    var ext = Path.GetExtension(pfile).ToLower();
+                    if (ext == ".exe" || ext == ".cmd" || ext == ".bat") return pfile;
+                }
+                if (File.Exists(pfile + ".exe")) return pfile + ".exe";
+                if (File.Exists(pfile + ".cmd")) return pfile + ".cmd";
+                if (File.Exists(pfile + ".bat")) return pfile + ".bat";
+
+            }
+            return null;
+        }
 
 
         public static string GetArguments(params object[] arguments)
@@ -54,22 +90,96 @@ namespace Shaman.Runtime
             return "\"" + file + "\" " + GetArguments(arguments);
         }
 
+        public static StreamReader RunFromRaw(string workingDirectory, string command, params object[] args)
+        {
+            var psi = CreateProcessStartInfo(workingDirectory, command, args);
+            psi.RedirectStandardError = true;
+            psi.RedirectStandardOutput = true;
+            var stderr = new StringWriter();
+
+            var p = System.Diagnostics.Process.Start(psi);
+            
+            p.ErrorDataReceived += (s, e) =>
+            {
+                if (e.Data != null)
+                {
+                    stderr.Write(e.Data);
+                    stderr.Write('\n');
+                }
+            };
+            p.BeginErrorReadLine();
+
+          //  return p.StandardOutput;
+            var wrapped = new WrappedStreamReader(p.StandardOutput.BaseStream, p, stderr, psi);
+            return new StreamReader(wrapped, p.StandardOutput.CurrentEncoding, false);
+
+            
+        }
+        
+        public static IEnumerable<string> RunFromRawEnumerable(string workingDirectory, string command, params object[] args)
+        {
+            var reader = RunFromRaw(workingDirectory, command, args);
+            var used = new bool[1];
+            return MakeEnumerable(reader, used);
+        }
+        
+        private static IEnumerable<string> MakeEnumerable(TextReader reader, bool[] used)
+        {
+            if(used[0]) throw new InvalidOperationException("GetEnumerator() can only be called once on process outputs.");
+            used[0] = true;
+            using(reader)
+            {
+                while(true)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null) break;
+                    yield return line;
+                }
+            }
+        }
+
+
+
+
         public static string RunFrom(string workingDirectory, string command, params object[] args)
         {
             var psi = CreateProcessStartInfo(workingDirectory, command, args);
             psi.RedirectStandardError = true;
             psi.RedirectStandardOutput = true;
 
+            var allOutput = new StringWriter();
+            var stderr = new StringWriter();
             using (var p = System.Diagnostics.Process.Start(psi))
             {
-                var output = p.StandardOutput.ReadToEnd();
-                var err = p.StandardError.ReadToEnd();
+                p.OutputDataReceived += (s, e) =>
+                {
+                    if (e.Data != null)
+                    {
+                        allOutput.Write(e.Data);
+                        allOutput.Write('\n');
+                    }
+                };
+                p.ErrorDataReceived += (s, e) =>
+                {
+                    if (e.Data != null)
+                    {
+                        allOutput.Write(e.Data);
+                        allOutput.Write('\n');
+                        stderr.Write(e.Data);
+                        stderr.Write('\n');
+                    }
+                };
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
                 p.WaitForExit();
 
                 if (p.ExitCode != 0)
+                {
+                    var err = stderr.ToString();
+                    if (string.IsNullOrWhiteSpace(err)) err = allOutput.ToString();
                     throw new ProcessException(p.ExitCode, err, string.Format("Execution of {0} with arguments {1} failed with exit code {2}: {3}", psi.FileName, psi.Arguments, p.ExitCode, err));
-
-                return output;
+                }
+                return allOutput.ToString();
             }
         }
 
@@ -134,6 +244,84 @@ namespace Shaman.Runtime
             {
                 var v = Convert.ToString(Value, CultureInfo.InvariantCulture);
                 return Name + (ContainsSpecialCharacters(v) ? "\"" + v + "\"" : v);
+            }
+        }
+
+        private class WrappedStreamReader : Stream
+        {
+            private Stream baseStream;
+            private Process p;
+            private StringWriter stderr;
+            private ProcessStartInfo psi;
+
+            public WrappedStreamReader(Stream baseStream, Process p, StringWriter stderr, ProcessStartInfo psi)
+            {
+                this.baseStream = baseStream;
+                this.p = p;
+                this.stderr = stderr;
+                this.psi = psi;
+            }
+
+            public override bool CanRead { get { return true; } }
+
+            public override bool CanSeek { get { return false; } }
+
+            public override bool CanWrite { get { return false; } }
+
+            public override long Length { get { throw new NotSupportedException(); } }
+
+            public override long Position { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+
+            public override void Flush()
+            {
+                throw new NotSupportedException();
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+
+                var k = baseStream.Read(buffer, offset, count);
+                if (k == 0)
+                {
+                    p.WaitForExit();
+                    if (p.ExitCode != 0)
+                    {
+                        var err = stderr.ToString();
+                        throw new ProcessException(p.ExitCode, err, string.Format("Execution of {0} with arguments {1} failed with exit code {2}: {3}", psi.FileName, psi.Arguments, p.ExitCode, err));
+                    }
+                }
+                return k;
+
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    try
+                    {
+                        if (!p.HasExited) p.Kill();
+                    }
+                    catch
+                    {
+                    }
+                    p.Dispose();
+                }
+            }
+
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                throw new NotSupportedException();
+            }
+
+            public override void SetLength(long value)
+            {
+                throw new NotSupportedException();
+            }
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                throw new NotSupportedException();
             }
         }
     }
